13948833edbc58722e1e0a19d7a15ce20bfbc8bf
Shreyansh Padhiyar <shreyanshpadhiyar33@gmail.com>
2025-02-17 17:02:11 +0530

    Fix cors origin

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -5,8 +5,7 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
+    origin: "*",
 }))
 app.use(express.json({limit: "20kb"}))
 app.use(express.urlencoded({ extended: true }))
