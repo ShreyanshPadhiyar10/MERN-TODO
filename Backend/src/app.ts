fa13084afb6380eff2ef0ee4afa87c16ed07f632
Shreyansh Padhiyar <shreyanshpadhiyar33@gmail.com>
2025-02-19 20:31:16 +0530

    change in app

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -6,8 +6,8 @@ const app = express()
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
-    // credentials: true,
-    // methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
 }))
 app.use(express.json({limit: "20kb"}))
 app.use(express.urlencoded({ extended: true }))
