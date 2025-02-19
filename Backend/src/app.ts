57d05c25cfcbfd043e069d4074e83ff596b34546
Shreyansh Padhiyar <shreyanshpadhiyar33@gmail.com>
2025-02-19 23:08:42 +0530

    credentials changes

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -5,10 +5,12 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: "https://mern-todo-pink.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
 }))
+app.options("*", cors()); 
 app.use(express.json({limit: "20kb"}))
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
