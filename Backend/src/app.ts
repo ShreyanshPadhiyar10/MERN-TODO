32ba15d6fef37c3b4415211739b68978aba290d3
Shreyansh Padhiyar <shreyanshpadhiyar33@gmail.com>
2025-02-20 22:38:31 +0530

    back to previous

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,10 +1,8 @@
 import express from "express"
-import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser"
-import { DBConnect } from "./db/connection";
 
-export const app = express()
+const app = express()
 
 app.use(cors({
     origin: "https://mern-todo-pink.vercel.app",
@@ -24,22 +22,10 @@ app.use((req, res, next) => {
     next();
 });
 
-
 app.use(express.json({ limit: "20kb" }))
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
-dotenv.config({
-    path: "./env"
-})
-
-DBConnect()
-    .then(() => {
-        app.listen(process.env.PORT || 8000, () => {
-            console.log(`Server is running on port ${process.env.PORT || 8000}`)
-        })
-    })
-    .catch((err: any) => console.log("Connection error", err));
 
 import userRouter from "./routes/user.routes"
 import todoRouter from "./routes/todo.routes"
@@ -47,4 +33,4 @@ import todoRouter from "./routes/todo.routes"
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/todo", todoRouter)
 
-// export { app }
+export { app }
