8eda6bcb5f3b37a9deff8e98e89201723b90e944
Shreyansh Padhiyar <shreyanshpadhiyar33@gmail.com>
2025-02-20 17:22:15 +0530

    changes in vercel

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -11,13 +11,13 @@ app.use(cors({
     allowedHeaders: ["Content-Type", "Authorization"],
 }))
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://mern-todo-pink.vercel.app");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "https://mern-todo-pink.vercel.app");
+//     res.header("Access-Control-Allow-Credentials", "true");
+//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//     next();
+// });
 
 app.options("*", (req, res) => {
     res.status(204).end();
