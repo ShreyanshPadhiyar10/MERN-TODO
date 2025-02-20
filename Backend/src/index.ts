bb9a9255273c624e18e52b40417d24662908cc88
Shreyansh Padhiyar <shreyanshpadhiyar33@gmail.com>
2025-02-20 17:05:06 +0530

    app to index

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,10 +1,30 @@
 import dotenv from "dotenv";
 import { app } from "./app";
+import cors from "cors";
 import { DBConnect } from "./db/connection";
 
 dotenv.config({
     path: "./env"
 })
+
+app.use(cors({
+    origin: "https://mern-todo-pink.vercel.app",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}))
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://mern-todo-pink.vercel.app");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
+app.options("*", (req, res) => {
+    res.status(204).end();
+});
 
 DBConnect()
     .then(() => {
