3a4b4026ddb828041a850323440ad45fd9615155
Shreyansh Padhiyar <shreyanshpadhiyar33@gmail.com>
2025-02-20 16:53:09 +0530

    changes in app of backend

--- Backend/dist/app.js
+++ Backend/dist/app.js
@@ -10,10 +10,21 @@ const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const app = (0, express_1.default)();
 exports.app = app;
 app.use((0, cors_1.default)({
-    origin: process.env.CORS_ORIGIN,
+    origin: "https://mern-todo-pink.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
 }));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://mern-todo-pink.vercel.app");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+app.options("*", (req, res) => {
+    res.status(204).end();
+});
 app.use(express_1.default.json({ limit: "20kb" }));
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, cookie_parser_1.default)());
--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -10,8 +10,20 @@ app.use(cors({
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
 }))
-app.options("*", cors()); 
-app.use(express.json({limit: "20kb"}))
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://mern-todo-pink.vercel.app");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
+app.options("*", (req, res) => {
+    res.status(204).end();
+});
+
+app.use(express.json({ limit: "20kb" }))
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
